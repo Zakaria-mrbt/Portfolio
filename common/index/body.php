cd5ae3efa54ee82618e9ad66ae8fa5e51fcedda6
13010-30-07 <13010-30-07@exchange.ad.afpanet>
2022-03-29 13:54:26 +0200

    ajoute projet avant premier rangement

--- common/index/body.php
+++ common/index/body.php
@@ -1,88 +1,160 @@
-
 <body class="bg-bgbody">
     <main>
-    <div class="home">
-        <img src="common/image/flame-280r.png" alt="ur1" class="robot_ur1"></img>
+        <div class="home">
+            <img src="common/image/flame-280r.png" alt="ur1" class="robot_ur1"></img>
             <h3>Développeur Web et Web mobile</h3>
-            
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="common/image/pixeltrue-icons-building-website-3.png" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="common/image/pixeltrue-icons-seo-ads-on-google.png" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="common/image/pixeltrue-icons-seo-ads-on-google-1.png" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-            <h2>Création de sites web</h2>
-            <p>Site vitrine, corporate, événementiel, e-commerce</p>
-            <img src="common/image/flame-victory-r" alt="flame-victory-r" class="robot_dr1">
-    </div>
-    <div class="about">
-      <div class="blue">
-        
-      </div>
-    </div>
-    </main>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <p>Lorem ipsum dolor sit amet.</p>
-    <script src="common/js/app.js"></script>
-    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
-</body>
+            <div class="container-fluid">
+                <div
+                    id="carouselExampleControls"
+                    class="carousel slide"
+                    data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img
+                                src="common/image/pixeltrue-icons-building-website-3.png"
+                                class="d-block w-100"
+                                alt="..."></div>
+                            <div class="carousel-item">
+                                <img
+                                    src="common/image/pixeltrue-icons-seo-ads-on-google.png"
+                                    class="d-block w-100"
+                                    alt="..."></div>
+                                <div class="carousel-item">
+                                    <img
+                                        src="common/image/pixeltrue-icons-seo-ads-on-google-1.png"
+                                        class="d-block w-100"
+                                        alt="..."></div>
+                                </div>
+                                <button
+                                    class="carousel-control-prev"
+                                    type="button"
+                                    data-bs-target="#carouselExampleControls"
+                                    data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button
+                                    class="carousel-control-next"
+                                    type="button"
+                                    data-bs-target="#carouselExampleControls"
+                                    data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                        </div>
 
+                        <h2>Création de sites web</h2>
+                        <p>Site vitrine, corporate, événementiel, e-commerce</p>
+                        <img src="common/image/flame-victory-r" alt="flame-victory-r" class="robot_dr1"></div>
+
+                        <div class="about">
+                            <div class="blue">
+                                <div class="title">
+                                    <h1>About
+                                    </h1>
+                                    <div class="span_orange"></div>
+                                </div>
+                                <div class="about_grid">
+                                    <section>
+                                        <h2>Développeur web ma passion et surtout mon métier</h2>
+                                        <p>Le développeur web / la développeuse réalise l'ensemble des fonctionnalités
+                                            techniques d'un site ou d'une application web.</p>
+                                        <h2>Ma passion pour le Développeur web
+                                        </h2>
+                                        <p>Toujours passionné d'informatique, c'est seulement en 2022 que je me lance
+                                            dans une formation Développeur web et web mobile au sein de l'AFPA saint Jérôme
+                                            a Marseille en changeant radicalement de cursus afin de pouvoir m'épanouir dans
+                                            ce domaine
+                                        </p>
+                                    </section>
+                                    <div class="img_about">
+                                        <img src="common/image/surr-675.png" alt="about" class="img_about_1"></div>
+                                        <section>
+                                            <h2>Expérience acquise aux files des projets produis.
+                                            </h2>
+                                            <p>Dès le commencement de cette formation, je mis toutes les chances de m'ont
+                                                coter afin de pouvoir exploiter pleinement les connaissances acquises tout au
+                                                long en mettant en place des projet me permettant de mieux comprendre les
+                                                attentes des futur client et de répondre le plus précisément au besoin/probleme
+                                                rencontré.
+                                            </p>
+
+                                            <img src="common/image/flame-281.png" alt="flame-281" class="img_about_2">
+                                                <p>
+                                                    Du site vitrine au site e-commerce ou a des projet plus complexe, je vous
+                                                    propose un développement qui réponde a toutes vos attentes et besoins.
+                                                </p>
+                                            </section>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="services">
+                                    <div class="title">
+                                        <h1>Services
+                                        </h1>
+                                        <div class="span_orange"></div>
+                                    </div>
+                                    <div class="services_main">
+                                        <div class="cards_one">
+                                            <img src="common/image/flame-success.gif" alt="flame-success" class="gif">
+
+                                                <img src="common/image/cards1.png" alt="cards1" class="cards_logo">
+                                                    <h1>Interface d'administration</h1>
+                                                    <p>Outils spécifiques au bon fonctionnement de votre entreprise.</p>
+
+                                                </div>
+                                                <div class="cards_two">
+                                                    <img src="common/image/flame-design-science.gif" alt="flame-success" class="gif">
+                                                        <img src="common/image/cards2.png" alt="cards2" class="cards_logo">
+                                                            <h1>GRAPHIQUE ET WEBDESIGN</h1>
+                                                            <p>Logo, template WEB, newsletters</p>
+                                                        </div>
+                                                        <div class="cards_one">
+                                            <img src="common/image/flame-82.gif" alt="flame-success" class="gif">
+
+                                                <img src="common/image/cards3.png" alt="cards3" class="cards_logo">
+                                                    <h1>Interface d'administration</h1>
+                                                    <p>Outils spécifiques au bon fonctionnement de votre entreprise.</p>
+
+                                                </div>
+                                                <div class="cards_two">
+                                                    <img src="common/image/flame-searching.gif" alt="flame-success" class="gif">
+                                                        <img src="common/image/cards4.png" alt="cards4" class="cards_logo">
+                                                            <h1>GRAPHIQUE ET WEBDESIGN</h1>
+                                                            <p>Logo, template WEB, newsletters</p>
+                                                        </div>
+                                                        <div class="cards_one">
+                                            <img src="common/image/flame-remote-working.gif" alt="flame-success" class="gif">
+
+                                                <img src="common/image/cards5.png" alt="cards5" class="cards_logo">
+                                                    <h1>Interface d'administration</h1>
+                                                    <p>Outils spécifiques au bon fonctionnement de votre entreprise.</p>
+
+                                                </div>
+                                                <div class="cards_two">
+                                                    <img src="common/image/flame-uploading.gif" alt="flame-success" class="gif">
+                                                        <img src="common/image/cards6.png" alt="cards6" class="cards_logo">
+                                                            <h1>GRAPHIQUE ET WEBDESIGN</h1>
+                                                            <p>Logo, template WEB, newsletters</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="portfolio">
+                                                    <div class="blue"><div class="title">
+                                                        <hi>
+                                                            PORTFOLIO
+                                                        </hi>
+                                                        <div class="span_orange"></div>
+                                                    </div>
+                                                    
+                                                    </div>
+                                                    
+                                                </div>
+                                            </main>
+                                            <script src="common/js/app.js"></script>
+                                            <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+                                        </body>
